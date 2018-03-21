0082f76d3c7650d5a3dec992a28c1f0451252dbc
Scott Leberknight <scottleber@gmail.com>
2018-03-21 09:56:43 -0400

    Chapter 3 - contains reducing operator and some collection operators
    (toList, toSortedList, toMap, toMultimap)

---
+++ src/main/kotlin/ch03/Ex28Contains.kt
@@ -0,0 +1,13 @@
+package ch03
+
+import io.reactivex.Observable
+
+
+fun main(args: Array<String>) {
+
+    Observable.range(1, 10_000)
+//            .doOnNext { println("Evaluating: $it") }
+            .contains(9563)
+            .subscribe { containedIn -> println("Contained in range? $containedIn") }
+
+}
