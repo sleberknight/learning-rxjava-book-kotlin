a3a6c50e67069e1419072f61c370dd20040479d1
Scott Leberknight <scottleber@gmail.com>
2018-03-21 16:18:38 -0400

    Chapter 3 - collect() on mutable Set and using Guava ImmutableList.Builder

---
+++ src/main/kotlin/ch03/Ex33CollectGuavaImmutableList.kt
@@ -0,0 +1,29 @@
+package ch03
+
+import com.google.common.collect.ImmutableList
+import io.reactivex.Observable
+
+
+/**
+ * As with Ex32Collect.kt I could not figure out how to just use constructor and method references
+ * as in the Java example where collect looks like:
+ *
+ * .collect(ImmutableList::builder, ImmutableList.Builder::add)
+ *
+ * Again, probably my beginner level of Kotlin knowledge...
+ */
+fun main(args: Array<String>) {
+
+    // 1. Method reference in map
+    Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+            .collect({ ImmutableList.Builder<String>() }, { set, next -> set.add(next) })
+            .map(ImmutableList.Builder<String>::build)
+            .subscribe { set -> println("Result: $set") }
+
+    // 2. Lambda in map...nicer
+    Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+            .collect({ ImmutableList.Builder<String>() }, { set, next -> set.add(next) })
+            .map { it.build() }
+            .subscribe { set -> println("Result: $set") }
+
+}
